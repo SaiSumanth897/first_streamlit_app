01ba47ea182900419ac8e90c4a3cbedb2cf0f394
SaiSumanth897 <140947890+SaiSumanth897@users.noreply.github.com>
2023-07-30 19:37:16 +0530

    Update streamlit_app.py
    checking the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Idli & Dosa')
-streamlit.text ('Vada Poori Chapathi')
+streamlit.text ('Vada, Poori, Chapathi')
 streamlit.text('Pani poori vada paav sandwich')
