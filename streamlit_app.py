04dfc7351925003133b610747711cac93e314979
SaiSumanth897 <140947890+SaiSumanth897@users.noreply.github.com>
2023-07-30 19:53:48 +0530

    Update streamlit_app.py
    import fruits data from S3 to our application

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('1.🥗 Idli, Dosa')
 streamlit.text ('2. 🥣 Vada, Poori, Chapathi')
 streamlit.text('3. 🍞 Panipoori, vadapaav, sandwich')
 streamlit.header(' Fruit Juices Menu')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
