2c35a96b245f5fdae53e1c59302c2d01923ef6c7
SaiSumanth897 <140947890+SaiSumanth897@users.noreply.github.com>
2023-07-30 19:45:36 +0530

    Update streamlit_app.py
    increased the list of items to the menu(Fruit Juices)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('1. Idli, Dosa')
-streamlit.text ('2. Vada, Poori, Chapathi')
-streamlit.text('3. Panipoori, vadapaav, sandwich')
+streamlit.text('1.🥗 Idli, Dosa')
+streamlit.text ('2. 🥣 Vada, Poori, Chapathi')
+streamlit.text('3. 🍞 Panipoori, vadapaav, sandwich')
+streamlit.header(' Fruit Juices Menu')
